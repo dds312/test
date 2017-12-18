f121730fd65fd06da0f07110eaf8753a63fcfd3d
dds312 <32283966+dds312@users.noreply.github.com>
2017-12-18 12:20:54 +0800

    Revert "Revert "first""
    This reverts commit 02bf4fcb50dbfcc138983252f9913f6f8ce5ef88.

---
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+    <a href="http://www.miitbeian.gov.cn/" rel="external nofollow" target="_blank">
+        <?php echo get_option( 'zh_cn_l10n_icp_num' );?>
+    </a>
+</div><!-- .site-info -->
